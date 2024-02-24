f20b9e1ae568ceaa5c9a6edaef00f1e7389f20bf
eddievin <thokerunga200@gmail.com>
2024-02-25 01:33:18 +0800

    updated code

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Image } from 'react-native';
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.mainContainer}>
+    <SafeAreaView>
+        <View style={styles.pageHeader}>
+        <TouchableOpacity><MaterialIcons style={styles.icon} name="arrow-back"  /></TouchableOpacity>
+          <Text style={styles.signUpText}>Sign Up</Text>
+          <TouchableOpacity><MaterialIcons style={styles.icon} name="close"  /></TouchableOpacity>
+        </View>
+      </SafeAreaView>
+
+      <View style={styles.imageContainer}>
+      <Image 
+      source = {require('./assets/img1.png')}
+      style={{height:'40%', width:'40%', resizeMode: 'contain',}}
+       />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+
+  //Header
+  pageHeader: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    margin: "5%",
+  },
+  icon:{
+    fontSize:30,
+    fontWeight:'bold',
+  },
+
+  //Image
+  imageContainer:{
+alignItems:'center',
+  },
+ 
 });
